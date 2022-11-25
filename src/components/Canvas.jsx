de2c5883f864043cff4f0ca893566e299779f50f
Oleg <35769958+trudoholic@users.noreply.github.com>
2022-11-25 15:06:56 +0300

    onMouseMove

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,24 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Sky from './Sky';
 import Ground from './Ground';
 import CannonBase from './CannonBase';
 import CannonPipe from './CannonPipe';
 
-const Canvas = () => {
+const Canvas = (props) => {
     const viewBox = [window.innerWidth / -2, 100 - window.innerHeight, window.innerWidth, window.innerHeight];
     return (
         <svg
             id="aliens-go-home-canvas"
-            // preserveAspectRatio="xMaxYMax none"
+            preserveAspectRatio="xMaxYMax"
+            onMouseMove={props.trackMouse}
             viewBox={viewBox}
         >
             <Sky />
             <Ground />
-            {/*<circle cx={0} cy={0} r={50} />*/}
-            <CannonPipe rotation={45} />
+            <CannonPipe rotation={props.angle} />
             <CannonBase />
         </svg>
     );
+};
+
+Canvas.propTypes = {
+    angle: PropTypes.number.isRequired,
+    trackMouse: PropTypes.func.isRequired,
 };
 
 export default Canvas;
